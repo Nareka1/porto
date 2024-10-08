cf0b6b5ff9909d703cf333ade4421b6d0133b82f
Nareka1 <134252479+Nareka1@users.noreply.github.com>
2024-10-08 12:46:19 +0700

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,7 @@
+let input= document.querySelector('input');
+let button = document.querySelector('button');
+
+button.addEventListener('click',function(){
+    let isi = input.value;
+    console.log(isi);
+}) // mouseover(saat mouse masuk), mouseout(saat maouse keluar)
