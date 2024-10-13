7f0e4d7740487b655a51d0033350c5feec2a4f3c
Nareka1 <>
2024-10-13 20:57:28 +0700

    bulk update and file issues

--- contact.js
+++
@@ -1,7 +0,0 @@
-let input= document.querySelector('input');
-let button = document.querySelector('button');
-
-button.addEventListener('click',function(){
-    let isi = input.value;
-    console.log(isi);
-}) // mouseover(saat mouse masuk), mouseout(saat maouse keluar)
--- index.js
+++
@@ -1,6 +0,0 @@
-for(var i =0 ; i<10;i+=1){
-    console.log('bajurit')
-}
-const resume = ['amir','bahrudin','samsuri'];
-resume[0]='rulah';
-console.log(resume)
